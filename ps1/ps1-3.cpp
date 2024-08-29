c113914557cdaf368c0d199721666737bd729a30
akameswa <adithya.k.nitt@gmail.com>
2024-08-29 07:47:35 -0400

    add reqd headers

--- ps1/ps1-3.cpp
+++ ps1/ps1-3.cpp
@@ -1,4 +1,7 @@
-﻿#include <iostream>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
 
 int main()
 {
@@ -6,17 +9,17 @@ int main()
     printf("ax + by = c \ndx + ey = f \nEnter a b c d e f:");
     scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
 
-    float abs_val = ((a * e) - (b * d));
-    if (abs_val < 0.000001)
+    double det = std::abs((a * e) - (b * d));
+    if (det < 0.000001)
     {
         printf("No Solution");
     }
     else
     {
-        int x = ((e * c) - (b * f)) / abs_val;
-        int y = ((a * f) - (c * d)) / abs_val;
-        printf("x=%d y=%d", x, y);
+        double x = ((e * c) - (b * f)) / det;
+        double y = ((a * f) - (c * d)) / det;
+        printf("x=%f y=%f", x, y);
     }
 
+    return 0;
 }
-
